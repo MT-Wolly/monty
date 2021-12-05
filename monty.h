a121c832776d0e6e2a833a202c9553841f700200
Mustapha Wolly <wollymustapha@gmail.com>
2021-12-04 20:38:23 -0500

    header file

--- monty.h
+++ monty.h
@@ -61,7 +61,7 @@ void add(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void divide(stack_t **stack, unsigned int line_number);
-
+void mod(stack_t **stack, unsigned int line_number);
 
 /* get function */
 
