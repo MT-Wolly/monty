b65e344d0a0dc7d4b11fb26f9a98f02d6fc34db0
Mustapha Wolly <wollymustapha@gmail.com>
2021-12-04 20:13:47 -0500

    opcode insructions 2

---
+++ opcode_instructions2.c
@@ -0,0 +1,111 @@
+#include "monty.h"
+
+/**
+* nop - does nothing
+* @stack: pointer to the beginning of stack
+* @line_number: the line number of the bytecode
+* Return: Nothing
+*/
+
+void nop(stack_t **stack, unsigned int __attribute__((__unused__))line_number)
+{
+	(void)stack;
+}
+
+/**
+* add - adds the first two elements at the top of stack
+* @stack: pointer to the beginning of the stack
+* @line_number: the line number of the byte code
+* Return: Nothing
+*/
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next, *head = *stack;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	next = head->next;
+	next->n = head->n + head->next->n;
+	next->prev = NULL;
+	*stack = next;
+	free(head);
+}
+
+/**
+* sub - subtracts the first element from the second in the stack
+* @stack: pointer to the beginning of the stack
+* @line_number: the line number of the byte code
+* Return: Nothing
+*/
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next, *head = *stack;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	next = head->next;
+	next->n = head->next->n - head->n;
+	next->prev = NULL;
+	*stack = next;
+	free(head);
+}
+
+/**
+* mul - multiplies the first two element of the stack
+* @stack: pointer to the beginning of the stack
+* @line_number: the line number of the byte code
+* Return: Nothing
+*/
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next, *head = *stack;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	next = head->next;
+	next->n = head->next->n * head->n;
+	next->prev = NULL;
+	*stack = next;
+	free(head);
+}
+
+/**
+* divide - divides the second element from the first in the stack
+* @stack: pointer to the beginning of the stack
+* @line_number: the line number of the byte code
+* Return: Nothing
+*/
+
+void divide(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next, *head = *stack;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (head->n == 0)
+	{
+		dprintf(STDERR_FILENO, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	next = head->next;
+	next->n = head->next->n / head->n;
+	next->prev = NULL;
+	*stack = next;
+	free(head);
+}
+
